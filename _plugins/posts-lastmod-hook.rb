3e3bc15493b65bdceb6efffbad7a2455d75857c5
wycIT <1954026018@qq.com>
2026-05-17 20:16:01 +0800

    更新第一篇博客

--- _plugins/posts-lastmod-hook.rb
+++ _plugins/posts-lastmod-hook.rb
@@ -4,11 +4,15 @@
 
 Jekyll::Hooks.register :posts, :post_init do |post|
 
-  commit_num = `git rev-list --count HEAD "#{ post.path }"`
+  begin
+    commit_num = `git rev-list --count HEAD "#{ post.path }"`
 
-  if commit_num.to_i > 1
-    lastmod_date = `git log -1 --pretty="%ad" --date=iso "#{ post.path }"`
-    post.data['last_modified_at'] = lastmod_date
+    if commit_num.to_i > 1
+      lastmod_date = `git log -1 --pretty="%ad" --date=iso "#{ post.path }"`.strip
+      post.data['last_modified_at'] = lastmod_date unless lastmod_date.empty?
+    end
+  rescue SystemCallError
+    # Git metadata is optional; don't fail the whole site when unavailable.
   end
 
 end
